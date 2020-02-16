3712597303aa67c92846e992911d688bd630eebc
Fred Valverde <fredvalverde@MacBookPro.local>
2020-02-15 20:06:58 -0600

    .

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/custom.css';
-// import registerServiceWorker from './registerServiceWorker';
 
 import { Left, Top, Footer } from './js/navigation';
 import history from './js/history';
@@ -12,4 +11,3 @@ import AppFactory from './js/App';
 const App = AppFactory(React, Router, history, Left, Top, Footer, PageContent, Menu);
 
 ReactDOM.render(<App/>, document.getElementById('root'));
-// registerServiceWorker();
